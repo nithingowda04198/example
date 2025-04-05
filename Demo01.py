5c0276f0e86148ebea03d92dcd26e4d57840be52
Nithin041297 <nithin041297@gmail.com>
2025-04-05 21:54:53 +0530

    Add files via upload

---
+++ Demo01.py
@@ -0,0 +1,21 @@
+a = 2
+b = 4
+int = 4
+str1 = "Nithin"
+str2 = "Gowda"
+fullname = str1 + " " + str2
+c = a+b
+
+#printing and a+b
+print(c)
+print("printing full name = " + fullname) 
+
+#printing condition statment
+
+if c == 4:
+
+        print(fullname)
+
+else:
+
+    print("print statment =" + fullname)
